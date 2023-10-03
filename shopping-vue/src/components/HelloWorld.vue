64aa4d95ba753446c61b82c30d1c2590c87ac82c
GalileoGuilherme <76979588+GalileoGuilherme@users.noreply.github.com>
2023-10-03 15:55:13 -0300

    update

--- shopping-vue/src/components/HelloWorld.vue
+++ shopping-vue/src/components/HelloWorld.vue
@@ -1,143 +1,26 @@
 <template>
   <div>
-    <h1 class="header">{{ msg }}</h1>
-    <div v-if="loading" class="loader">Carregando...</div>
-    <ul class="product-list">
-      <li v-for="product in products" :key="product.id" class="product-item">
-        <div class="product-image">
-          <img
-            :src="product.image"
-            :alt="product.title"
-            width="100"
-            height="100"
-          />
-        </div>
-        <div class="product-details">
-          <h2 class="product-title">{{ product.title }}</h2>
-          <p class="product-price">Preço: R$ {{ product.price }}</p>
-          <button @click="showProductDetails(product)">Mostrar Detalhes</button>
-        </div>
+    <!-- Renderize os produtos selecionados aqui -->
+    <ul>
+      <li v-for="product in getSelectedProducts" :key="product.id">
+        {{ product.title }}
       </li>
     </ul>
-
-    <!-- Modal -->
-    <div class="modal" v-if="showModal">
-      <div class="modal-content">
-        <h2>{{ selectedProduct.title }}</h2>
-        <p>{{ selectedProduct.description }}</p>
-        <button @click="closeModal">Fechar</button>
-      </div>
-    </div>
   </div>
 </template>
 
+
 <script>
-import axios from "axios";
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
-  props: {
-    msg: String,
-  },
-  data() {
-    return {
-      products: [],
-      showModal: false,
-      selectedProduct: null,
-      loading: false,
-    };
-  },
-  mounted() {
-    this.fetchProducts();
-  },
   methods: {
-    async fetchProducts() {
-      this.loading = true;
-      try {
-        const response = await axios.get("https://fakestoreapi.com/products");
-        this.products = response.data;
-      } catch (error) {
-        console.error("Erro ao buscar a lista de produtos:", error);
-      } finally {
-        this.loading = false;
-      }
-    },
-    showProductDetails(product) {
-      this.selectedProduct = product;
-      this.showModal = true;
-    },
-    closeModal() {
-      this.selectedProduct = null;
-      this.showModal = false;
-    },
+    // Use mapActions para chamar a ação do Vuex para atualizar os produtos selecionados
+    ...mapActions('products', ['updateSelectedProducts']),
+  },
+  computed: {
+    // Use mapGetters para acessar os produtos selecionados do Vuex
+    ...mapGetters('products', ['getSelectedProducts']),
   },
 };
 </script>
-
-<style scoped>
-.header {
-  font-size: 24px;
-  text-align: center;
-  margin-bottom: 20px;
-}
-
-.product-list {
-  list-style: none;
-  padding: 0;
-  width: 50%;
-  margin: 0 auto;
-}
-
-.product-item {
-  display: flex;
-  margin-bottom: 20px;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-}
-
-.product-image img {
-  border-radius: 5px;
-  margin-right: 20px;
-}
-
-.product-title {
-  font-size: 18px;
-  margin-bottom: 5px;
-}
-
-.product-price {
-  font-weight: bold;
-  color: #007bff;
-}
-
-.product-description {
-  font-size: 14px;
-  color: #555;
-}
-
-.modal {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.modal-content {
-  background-color: #fff;
-  width: 70%;
-  padding: 20px;
-  border-radius: 5px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
-}
-
-.loader {
-  text-align: center;
-  font-size: 20px;
-  margin-top: 50px;
-}
-</style>
--- shopping-vue/src/views/AboutView.vue
+++ shopping-vue/src/views/AboutView.vue
@@ -1,5 +1,103 @@
 <template>
-  <div class="about">
-    <h1>BackOffice</h1>
+  <div>
+    <h1 class="header">{{ msg }}</h1>
+    <div v-if="loading" class="loader">Carregando...</div>
+
+    <!-- Seletor de produtos com caixas de seleção -->
+    <div class="product-selector">
+      <h2>Selecione os produtos para mostrar na view do cliente:</h2>
+      <ul class="product-list">
+        <li v-for="product in products" :key="product.id" class="product-item">
+          <label>
+            <input
+              type="checkbox"
+              v-model="selectedProducts"
+              :value="product"
+            />
+            {{ product.title }}
+          </label>
+        </li>
+      </ul>
+    </div>
+
+    <button @click="sendSelectedProducts">Enviar Produtos Selecionados</button>
+
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+import { mapActions } from 'vuex';
+
+export default {
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      products: [],
+      showModal: false,
+      selectedProducts: [],
+      loading: false,
+    };
+  },
+  mounted() {
+    this.fetchProducts();
+  },
+  methods: {
+    async fetchProducts() {
+      this.loading = true;
+      try {
+        const response = await axios.get('https://fakestoreapi.com/products');
+        this.products = response.data;
+      } catch (error) {
+        console.error('Erro ao buscar a lista de produtos:', error);
+      } finally {
+        this.loading = false;
+      }
+    },
+    sendSelectedProducts() {
+      console.log(this.selectedProducts);
+      this.$store.dispatch("products/updateSelectedProducts", this.selectedProducts);
+      // this.selectedProducts = [];
+    },
+    ...mapActions('selectedProducts', ['updateSelectedProducts']),
+  },
+};
+</script>
+
+
+<style scoped>
+.product-selector {
+  text-align: left;
+}
+
+.product-item {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.product-item label {
+  margin-left: 10px;
+}
+
+.header {
+  font-size: 24px;
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.product-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  padding-left: 20px; /* Adicione um recuo à esquerda para os itens da lista */
+}
+
+.loader {
+  text-align: center;
+  font-size: 30px;
+  margin-top: 50px;
+}
+</style>
