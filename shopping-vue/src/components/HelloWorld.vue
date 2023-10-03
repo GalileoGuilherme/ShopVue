c000ec8d5ec95b23872645a2caa9442699fa02d7
GalileoGuilherme <76979588+GalileoGuilherme@users.noreply.github.com>
2023-10-03 16:12:58 -0300

    Update HelloWorld.vue

--- shopping-vue/src/components/HelloWorld.vue
+++ shopping-vue/src/components/HelloWorld.vue
@@ -1,13 +1,17 @@
+
 <template>
   <div>
-    <!-- Renderize os produtos selecionados aqui -->
-    <ul>
-      <li v-for="product in getSelectedProducts" :key="product.id">
-        {{ product.title }}
-      </li>
-    </ul>
+    <div class="product-grid">
+      <div v-for="product in getSelectedProducts" :key="product.id" class="product-item">
+        <img :src="product.image" :alt="product.title" class="product-image" />
+        <h3 class="product-title">{{ product.title }}</h3>
+        <p class="product-description">{{ product.description }}</p>
+        <p class="product-price">R$ {{ product.price }}</p>
+      </div>
+    </div>
   </div>
 </template>
+
 
 
 <script>
@@ -24,3 +28,38 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+
+.product-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  gap: 20px;
+}
+
+.product-item {
+  border: 1px solid #ccc;
+  padding: 10px;
+  text-align: center;
+}
+
+.product-image {
+  max-width: 100%;
+  height: auto;
+}
+
+.product-title {
+  font-size: 18px;
+  margin-top: 10px;
+}
+
+.product-description {
+  margin-top: 5px;
+}
+
+.product-price {
+  font-weight: bold;
+  margin-top: 5px;
+}
+
+</style>
